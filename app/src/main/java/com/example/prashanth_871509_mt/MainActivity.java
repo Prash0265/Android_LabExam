af9b30bde4bc06648874d776866f2d5a206a314d
Prash0265 <kmrprashanth92@gmail.com>
2022-12-05 19:53:37 -0500

    Implemted spinner, seekbar, Radio buttons

--- app/src/main/java/com/example/prashanth_871509_mt/MainActivity.java
+++ app/src/main/java/com/example/prashanth_871509_mt/MainActivity.java
@@ -3,27 +3,22 @@ package com.example.prashanth_871509_mt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
     Spinner spinner;
+    SeekBar seekbar;
+    TextView numOfDays;
 
     List<String> categories;
     @Override
@@ -31,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner);
+        seekbar = findViewById(R.id.seekBar);
+        numOfDays = findViewById(R.id.textView3);
 
         spinner.setOnItemSelectedListener(this);
         categories = new ArrayList<>();
@@ -45,18 +42,48 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         categories.add("Volvo");
         categories.add("Tesla");
 
+
+        // SEEKBAR CODE
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                Log.i("TAG","OnProgressChanged " + progress);
+                numOfDays.setText("Days: "+progress);
+//                value = progress;
+//                totalPrice.setText(""+(rate*value));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+
+        });
+
+        // SPINNER CODE
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
-    }
+        }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        }
 
-    }
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
 
-    }
+
+
+
+
 }
